f058d066972a032d3ec211f740d12221fa4fc8eb
koshi8bit <koshi8bit@mail.ru>
2022-04-07 14:09:31 +0700

    works ok for qt

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ import os
 def main():
     # os.chdir(r"C:\k8b\prog\python\prj\github-download\prj")
     # os.system(f"git clone -o gh {url}")
-    a = os.system(f"git rev-parse HEAD")
-    print(f"{a}=")
-    print("asd")
+    # a, b = os.system(f"git rev-parse HEAD")
+    import subprocess
+    a = os.popen(f"git rev-parse HEAD").read().strip()
+    print(f"{a}")
+    with open("git-commit-hash-template.h", "r") as f:
+        s = f.read().format(commit_hash=a)
+    with open("git-commit-hash.h", "w") as f:
+        f.write(s)
 
 
 if __name__ == '__main__':
